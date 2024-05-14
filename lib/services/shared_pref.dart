c137b6195aff925db5bb66eb834edc7b9b8c90c0
Sabinkc <135928502+Sabinkc@users.noreply.github.com>
2024-05-14 23:03:01 +0545

    Update shared_pref.dart

--- lib/services/shared_pref.dart
+++ lib/services/shared_pref.dart
@@ -1,21 +1,3 @@
-// // shared_pref.dart
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class SharedPref {
-//   static Future<String?> getLocation() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     return prefs.getString('location');
-//   }
-
-//   static Future<void> saveLocation(String location) async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     await prefs.setString('location', location);
-//   }
-// }
-
-
-
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefs {
